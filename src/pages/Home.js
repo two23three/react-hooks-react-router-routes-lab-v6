410cc5ec31ed82405fdcfac8346a641f65fb3cae
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-04 17:18:28 +0300

    doneeee

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() =>{
+    fetch("http://localhost:3000/movies")
+    .then(r => r.json())
+    .then(data => setMovies(data))
+    .catch(error => console.error(error))
+  }, [])
+  const movieList = movies.map(movie => <MovieCard key={movie.id} title={movie.title} id={movie.id}/>)
+
   return (
     <>
+    <h1>Home Page</h1>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        {movieList}
       </main>
     </>
   );
